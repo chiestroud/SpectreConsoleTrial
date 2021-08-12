e52da2c40f49e377811d5a2fbfeb57d1b5a7bd6e
Chie Stroud <urirunrunkoko@gmail.com>
2021-08-11 22:00:28 -0500

    Add project files.

---
+++ SpectreConsoleTrial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Spectre.Console;
+
+namespace SpectreConsoleTrial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnsiConsole.MarkupLine("[underline red]Hello[/] World!");
+            AnsiConsole.MarkupLine("[underline green]Hello[/] ");
+            AnsiConsole.MarkupLine("[bold]World[/]");
+            AnsiConsole.Markup("Hello :globe_showing_europe_africa:!");
+            AnsiConsole.Markup("[red]Foo[/] ");
+            AnsiConsole.Markup("[#ff0000]Bar[/] ");
+            AnsiConsole.Markup("[rgb(255,0,0)]Baz[/] ");
+        }
+    }
+}
